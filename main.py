9593fd76f0446dcfae9ce7856d6051f62b4e44b8
Programer <programer@gmail.com>
2026-06-27 23:33:13 +0300

    Исправил main

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from core.exceptions import (
     CredentialsError,
 )
 from fastapi.exceptions import RequestValidationError
-
+from core.config import settings
 app = FastAPI()
 app.include_router(application.router)
 app.include_router(user.router)
@@ -26,3 +26,11 @@ app.add_exception_handler(AlreadyExistsError, already_exists_handler)
 app.add_exception_handler(AuthenticationError, authentication_handler)
 app.add_exception_handler(CredentialsError, credentials_handler)
 app.add_exception_handler(RequestValidationError, validation_handler)  # type: ignore
+@app.on_event("startup")
+async def startup():
+    from sqlalchemy import create_engine
+    from db.base import Base
+    
+    sync_url = settings.DATABASE_URL.replace("+asyncpg", "")
+    engine = create_engine(sync_url)
+    Base.metadata.create_all(engine)
